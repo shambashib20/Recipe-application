da09c367e32762bad2341909a70dc8e64b1357bc
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2022-03-15 19:58:39 +0530

    updating the work

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import styles from '../styles/layout.module.css'
+
+const Layout = ({ children }) => {
+    return (
+        <div className={styles.container}>
+
+
+            <main className={styles.main}>
+                {children}</
+            main>
+
+        </div>
+    )
+}
+
+export default Layout; 
