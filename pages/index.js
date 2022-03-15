492c2d9e65061717f8ba17894815a78174c6ed8e
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2022-03-15 18:33:40 +0530

    updating the content.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>WebDev Newz</title>
+        <meta name='keywords' content='web development, prgramming' />
+      </Head>
+
       <h1>Welcome to next</h1>
     </div>
   )
